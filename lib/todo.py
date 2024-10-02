92e17ac4a0bff30373a55cdd8cb046d34c6056b3
Alfie Baxter <34726358+Colemakman@users.noreply.github.com>
2024-10-02 14:57:00 +0100

    Todo testing

--- lib/todo.py
+++ lib/todo.py
@@ -1,1 +1,12 @@
 # File: lib/todo.py
+
+def todo(string, filename):
+    # Open file and read lines into variable
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    
+    # See if 'string' is in any line
+    for line in lines:
+        if string in line:
+            return True
+    return False
--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,2 +1,17 @@
 # File: tests/test_todo.py
 
+import pytest
+from lib.todo import todo
+
+
+true_file = "notes/notes_todo.txt"
+false_file = "notes/notes_random.txt"
+
+def test_true_case():
+    result = todo("#TODO", true_file)
+    assert result == True 
+
+def test_false_case():
+    result = todo("#TODO", false_file)
+    assert result == False
+
